a6f889fa662211d3e826e2ea0e004c2078326805
mike9107 <9107mgarcia@gmail.com>
2022-12-05 02:19:03 -0500

    added vanilla go workaround

--- main.go
+++ main.go
@@ -5,3 +5,14 @@ import "goserve/cmd"
 func main() {
 	cmd.Execute()
 }
+
+// var (
+// 	port = flag.Int("port", 1234, "port to listen on")
+// 	path = flag.String("path", ".", "path to serve")
+// )
+
+// func main() {
+// 	flag.Parse()
+// 	log.Printf("serving %s at http://localhost:%d\n", *path, *port)
+// 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", *port), http.FileServer(http.Dir(*path))))
+// }
