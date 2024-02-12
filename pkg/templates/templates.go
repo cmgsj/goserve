72d29fc02671f0ec396972a100b3832bbbc99948
mike9107 <9107mgarcia@gmail.com>
2024-02-12 11:15:18 -0500

    display error status and message

--- pkg/templates/templates.go
+++ pkg/templates/templates.go
@@ -4,6 +4,7 @@ import (
 	_ "embed"
 	"html/template"
 	"io"
+	"sort"
 )
 
 var (
@@ -12,11 +13,16 @@ var (
 	indexTmpl = template.Must(template.New("index").Parse(indexHTML))
 )
 
-type Page struct {
-	Error       string
+type Index struct {
+	Error       *Error
 	Breadcrumbs []File
 	Files       []File
 	Version     string
+}
+
+type Error struct {
+	Status  string
+	Message string
 }
 
 type File struct {
@@ -26,6 +32,15 @@ type File struct {
 	IsDir bool
 }
 
-func ExecuteIndex(w io.Writer, page Page) error {
-	return indexTmpl.Execute(w, page)
+func ExecuteIndex(w io.Writer, i Index) error {
+	return indexTmpl.Execute(w, i)
+}
+
+func SortFiles(files []File) {
+	sort.Slice(files, func(i, j int) bool {
+		if files[i].IsDir != files[j].IsDir {
+			return files[i].IsDir
+		}
+		return files[i].Name < files[j].Name
+	})
 }
