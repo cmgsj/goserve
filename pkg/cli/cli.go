428b426d88143f469e1b2c478ed84bbba4bdbc56
mike9107 <9107mgarcia@gmail.com>
2024-06-15 23:33:19 -0400

    cli public default flag set

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -6,84 +6,80 @@ import (
 	"time"
 )
 
-var defaultFlagSet = NewFlagSet(os.Args[0], flag.ExitOnError)
-
-func Default() *FlagSet {
-	return defaultFlagSet
-}
+var DefaultFlagSet = NewFlagSet(os.Args[0], flag.ExitOnError)
 
 func EnvPrefix() string {
-	return defaultFlagSet.EnvPrefix()
+	return DefaultFlagSet.EnvPrefix()
 }
 
 func SetEnvPrefix(envPrefix string) {
-	defaultFlagSet.SetEnvPrefix(envPrefix)
+	DefaultFlagSet.SetEnvPrefix(envPrefix)
 }
 
 func Usage() {
-	defaultFlagSet.Usage()
+	DefaultFlagSet.Usage()
 }
 
 func SetUsage(usage func(*FlagSet)) {
-	defaultFlagSet.SetUsage(usage)
+	DefaultFlagSet.SetUsage(usage)
 }
 
 func PrintDefaults() {
-	defaultFlagSet.PrintDefaults()
+	DefaultFlagSet.PrintDefaults()
 }
 
 func NFlag() int {
-	return defaultFlagSet.NFlag()
+	return DefaultFlagSet.NFlag()
 }
 
 func NArg() int {
-	return defaultFlagSet.NArg()
+	return DefaultFlagSet.NArg()
 }
 
 func Arg(i int) string {
-	return defaultFlagSet.Arg(i)
+	return DefaultFlagSet.Arg(i)
 }
 
 func Args() []string {
-	return defaultFlagSet.Args()
+	return DefaultFlagSet.Args()
 }
 
 func StringFlag(name, usage string, required bool, defaults ...string) *Flag[string] {
-	return defaultFlagSet.StringFlag(name, usage, required, defaults...)
+	return DefaultFlagSet.StringFlag(name, usage, required, defaults...)
 }
 
 func BoolFlag(name, usage string, required bool, defaults ...bool) *Flag[bool] {
-	return defaultFlagSet.BoolFlag(name, usage, required, defaults...)
+	return DefaultFlagSet.BoolFlag(name, usage, required, defaults...)
 }
 
 func IntFlag(name, usage string, required bool, defaults ...int) *Flag[int] {
-	return defaultFlagSet.IntFlag(name, usage, required, defaults...)
+	return DefaultFlagSet.IntFlag(name, usage, required, defaults...)
 }
 
 func Int64Flag(name, usage string, required bool, defaults ...int64) *Flag[int64] {
-	return defaultFlagSet.Int64Flag(name, usage, required, defaults...)
+	return DefaultFlagSet.Int64Flag(name, usage, required, defaults...)
 }
 
 func UintFlag(name, usage string, required bool, defaults ...uint) *Flag[uint] {
-	return defaultFlagSet.UintFlag(name, usage, required, defaults...)
+	return DefaultFlagSet.UintFlag(name, usage, required, defaults...)
 }
 
 func Uint64Flag(name, usage string, required bool, defaults ...uint64) *Flag[uint64] {
-	return defaultFlagSet.Uint64Flag(name, usage, required, defaults...)
+	return DefaultFlagSet.Uint64Flag(name, usage, required, defaults...)
 }
 
 func Float64Flag(name, usage string, required bool, defaults ...float64) *Flag[float64] {
-	return defaultFlagSet.Float64Flag(name, usage, required, defaults...)
+	return DefaultFlagSet.Float64Flag(name, usage, required, defaults...)
 }
 
 func DurationFlag(name, usage string, required bool, defaults ...time.Duration) *Flag[time.Duration] {
-	return defaultFlagSet.DurationFlag(name, usage, required, defaults...)
+	return DefaultFlagSet.DurationFlag(name, usage, required, defaults...)
 }
 
 func Parsed() bool {
-	return defaultFlagSet.Parsed()
+	return DefaultFlagSet.Parsed()
 }
 
 func Parse() error {
-	return defaultFlagSet.Parse()
+	return DefaultFlagSet.Parse()
 }
