2d8c240a1f8a38549b25b57a439a7987601e9ae0
mike9107 <9107mgarcia@gmail.com>
2024-06-18 18:09:43 -0400

    file struct all fields non optional

--- pkg/files/file.go
+++ pkg/files/file.go
@@ -13,7 +13,7 @@ const (
 type File struct {
 	Path  string `json:"path"`
 	Name  string `json:"name"`
-	Size  string `json:"size,omitempty"`
+	Size  string `json:"size"`
 	IsDir bool   `json:"is_dir"`
 }
 
